089d62c4ff7c339d73ea47f318b6d25012f60e6f
k-omura <kousuke.jin.oomura@gmail.com>
2017-09-16 21:29:32 +0900

    Create EUSARToutput.ino

---
+++ Peripheral program/EUSARToutput.ino
@@ -0,0 +1,30 @@
+/*
+ * Arduino program
+ * 
+ */
+
+int i;
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.write(0b00000000);
+  delay(2000);
+
+  Serial.write(0b00110000);
+  delay(2000);
+
+  Serial.write(0b01110000);
+  delay(2000);
+
+  Serial.write(0b11110000);
+  delay(2000);
+
+  for (i = 0; i < 20; i++) {
+    Serial.write(0b11110000);
+    delay(50);
+    Serial.write(0b11100000);
+    delay(50);
+  }
+}
